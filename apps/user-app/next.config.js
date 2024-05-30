88b45a33962e99c25c39a441625726880eaefec3
Hardikx10 <119948004+Hardikx10@users.noreply.github.com>
2024-05-30 06:56:18 -0700

    Update next.config.js

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -2,19 +2,19 @@
 module.exports = {
   transpilePackages: ["@repo/ui"],
 };
-const {
-  withHydrationOverlay,
-} = require("next-hydration-overlay/next");
+// const {
+//   withHydrationOverlay,
+// } = require("next-hydration-overlay/next");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /** your config here */
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /** your config here */
+// };
 
-module.exports = withHydrationOverlay({
-  /**
-   * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
-   * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
-   */
-  appRootSelector: "#__next",
-})(nextConfig);
+// module.exports = withHydrationOverlay({
+//   /**
+//    * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
+//    * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
+//    */
+//   appRootSelector: "#__next",
+// })(nextConfig);
